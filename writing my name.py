23ae55299c08efdf55ccfc47dcd60b6a4c90ece4
nizar.grami <nizar.grami@gmail.com>
2022-11-16 19:56:00 +0100

    importing every method from the module turtle, i used : from turtle import *

--- writing my name.py
+++ writing my name.py
@@ -1,4 +1,6 @@
 # this program is intended to fdraw my name usin Python and the module turtle
 
-# importing the module turtle
-import turtle
+# importing the module turtle and all its methods
+from turtle import *
+
+
