67ca0d33018c0fee8e816bc57011510bd105c85a
nizar.grami <nizar.grami@gmail.com>
2022-11-16 20:00:11 +0100

    defining the screen object

--- writing my name.py
+++ writing my name.py
@@ -1,6 +1,10 @@
-# this program is intended to fdraw my name usin Python and the module turtle
+# this program is intended to draw my name usin Python and the module turtle
 
 # importing the module turtle and all its methods
 from turtle import *
+
+# defining the screen
+screen=turtle.Screen()
+bgcolor("white")
 
 
