507b7a89e7b93fe312be4ac908054b752ef9a5d9
nizar.grami <nizar.grami@gmail.com>
2022-11-20 10:47:19 +0100

    giving some orders to the turtle to draw moving forward and drawing the half of o a circle, then filling the drawing with black color

--- writing my name.py
+++ writing my name.py
@@ -20,6 +20,14 @@ mode("logo")
 # moving the turtle to a start point, located at the left edge of the screen
 goto (300,0)
 
+pendown() # order the turtle to trace
+begin_fill() # filling the future drawing after closing lines
+forward (300) # tracing a straight vertical line from the botton to to top
+circle(5,180) #  drawing half a circle to the left
+forward(300) #tracing back a straight vertical line from the top  to to bottom
+circle(5,180) # drawing the half of circle in order to close the drawing
+end_fill() #filling the drawing after the lines have been closed
+penup() # ordering the turtle to not to draw
 
 
 
